24c8f524357ba858d65b0cdbfa63065b9a3f5b12
cvpcs <root@cvpcs.org>
2010-08-29 22:01:46 -0500

    added libc-ext reference and boot path

--- hijack/Android.mk
+++ hijack/Android.mk
@@ -6,9 +6,11 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := hijack.c
 LOCAL_MODULE := hijack
 LOCAL_MODULE_TAGS := eng
-LOCAL_STATIC_LIBRARIES := libbusybox libclearsilverregex libm libcutils libc
+LOCAL_STATIC_LIBRARIES := libbusybox libclearsilverregex libm libcutils libc-ext libc
 LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_CFLAGS += -DBOARD_HIJACK_RECOVERY_PATH=\"$(BOARD_HIJACK_RECOVERY_PATH)\"
+LOCAL_CFLAGS += \
+	-DBOARD_HIJACK_BOOT_PATH=\"$(BOARD_HIJACK_BOOT_PATH)\"
+	-DBOARD_HIJACK_RECOVERY_PATH=\"$(BOARD_HIJACK_RECOVERY_PATH)\"
 include $(BUILD_EXECUTABLE)
 
 endif
