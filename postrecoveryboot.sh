0bcdce71a20aaa5a57ae4405c12f36a7d632ae5b
Austen Dicken <cvpcsm@gmail.com>
2011-06-16 14:18:20 -0500

    added some postrecoveryboot stuff for overwriting default.prop
    we need to overwrite default.prop so we get root in adb, among
    other things.
    
    for now just embed the overwrite into postrecoveryboot.sh
    
    Change-Id: I0b69aa8195f6e3bb40abf61bebfb57494b7e2f34

--- postrecoveryboot.sh
+++ postrecoveryboot.sh
@@ -19,6 +19,17 @@ mkdir -p tmp
 rm sdcard
 mkdir sdcard
 
+# overwrite default.prop so we get root adb
+cat > /default.prop <<EOF
+#
+# ADDITIONAL_DEFAULT_PROPERTIES
+#
+ro.secure=0
+ro.allow.mock.location=1
+ro.debuggable=1
+persist.service.adb.enable=1
+EOF
+
 # Restart with root hacked adbd
 echo msc_adb > /dev/usb_device_mode
 touch /tmp/recovery.log
