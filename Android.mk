LineageOS/android_device_motorola_shadow

shadow: Only inherit makefiles if building the device
This avoids generating device-specific files on other
builds.

Change-Id: I397261c1919631843dfd71309bb7c49cbb00ed1b

Android.mk
@@ -1,0 +1,3 @@
+ifeq ($(TARGET_DEVICE),shadow)
+    include $(all-subdir-makefiles)
+endif

MotoMagic/android_device_moto_shadow

Android.mk
@@ -1,0 +1,3 @@
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),shadow)
+    include $(all-subdir-makefiles)
+endif